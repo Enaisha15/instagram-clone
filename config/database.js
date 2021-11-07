57854340a7a28cdebb40a788cf31b36b2305e521
Enaisha <Enaisha15@gmail.com>
2021-11-06 21:54:03 -0400

    Enaisha insta app

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+
+const dbName = "instagram";
+
+module.exports = {
+  url: "mongodb+srv://Enaisha15:Jasmine123!@cluster0.jrgra.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
